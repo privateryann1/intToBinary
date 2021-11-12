486179a1db14d8d9969d18d2395824ca9aefbd04
Ryan <59742677+privateryann1@users.noreply.github.com>
2021-11-11 16:35:11 -0800

    Main files

---
+++ BinaryConversion/src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static int intToBinary(int x) {
+        int quotient = x / 2;
+        int a = 1;
+        while(quotient > 0) {
+            a++;
+            quotient = quotient / 2;
+        }
+
+        int[] remainders = new int[a];
+        remainders[0] = x % 2;
+        quotient = x / 2;
+
+        for (int i = 1; i < a; i++) {
+            remainders[i] = quotient % 2;
+            quotient = quotient / 2;
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < remainders.length; i++) {
+            result.append(remainders[remainders.length - (i + 1)]);
+        }
+
+        return Integer.parseInt(result.toString());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(intToBinary(3));
+    }
+
+}
